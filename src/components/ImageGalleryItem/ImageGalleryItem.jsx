85773ffa80c34000725543e25e67d189df68c368
helenserb <serbinenkoifbb@gmail.com>
2023-04-05 23:21:18 -0400

    modal in hooks

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import Modal from 'components/Modal/Modal';
+import {Modal} from 'components/Modal/Modal';
 import { useState } from 'react';
 
 import css from './ImageGalleryItem.module.css';
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,37 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css'
 
 const modalRoot = document.querySelector('#modal-root');
-export default class Modal extends Component{
 
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown)
-    };
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
-
-    handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
+export const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+      const handleKeyDown = e => {
+        if (e.code === 'Escape') {
+          onClose();
+        }
       };
     
-    handleBackdropClick = (e) => {
-        if (e.currentTarget === e.target) {
-            this.props.onClose();
-      }
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
     }
-    
-    render = () => {
-        const { children} = this.props;
-        
-        return (
-            createPortal(
-                <div className={css.Overlay} onClick={this.handleBackdropClick}>
-                    <div className={css.Modal}>{children}</div>
-                </div>, modalRoot)
-        )
-    }
+  };
+
+  return createPortal(
+    <div className={css.Overlay} onClick={handleBackdropClick}>
+      <div className={css.Modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
 }
 
 Modal.propTypes = {
