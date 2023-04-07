5f3005ca86958b9477a20cc7311cd5d55aef56be
helenserb <serbinenkoifbb@gmail.com>
2023-04-06 21:58:30 -0400

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -27,36 +27,6 @@ export const ImageGalleryItem = ({ id, webformatURL, largeImageURL, tags }) => {
     </li>
   );
 };
-// export default class ImageGalleryItem extends Component {
-//   // state = {
-  //   isShowModal: false,
-  // };
-
-  // toggleModal = () => {
-  //   this.setState(({ isShowModal }) => ({ isShowModal: !isShowModal }));
-  // };
-
-//   render = () => {
-//     const { isShowModal } = this.state;
-//     const { id, webformatURL, largeImageURL, tags } = this.props;
-
-//     return (
-//       <li key={id} className={css.ImageGalleryItem}>
-//         <img
-//           src={webformatURL}
-//           alt={tags}
-//           className={css.ImageGalleryItemImage}
-//           onClick={this.toggleModal}
-//         />
-//         {isShowModal && (
-//           <Modal onClose={this.toggleModal}>
-//             <img src={largeImageURL} alt={tags} />
-//           </Modal>
-//         )}
-//       </li>
-//     );
-//   };
-// }
 
 ImageGalleryItem.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
